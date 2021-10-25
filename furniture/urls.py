409b67851677eef5089285f8c8bfbf5d77d995bf
dmanojlo <dragojlo@net.hr>
2021-10-25 16:18:39 +0200

    Ini

--- furniture/urls.py
+++ furniture/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include, re_path
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
-from .views import mima
+from .views import mima, emezzeta
 
 app_name = 'furniture' # za url putanju do appa
 
 urlpatterns = [
      path('mima/', mima, name='mima'),
+     path('emezzeta/', emezzeta, name='emezzeta'),
 
 ]

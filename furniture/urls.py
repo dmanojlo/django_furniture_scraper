5bd07e1d548904a6ddabacec2405248308975172
dmanojlo <dragojlo@net.hr>
2021-11-02 16:34:02 +0100

    Ini

--- furniture/urls.py
+++ furniture/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include, re_path
 from django.urls import reverse_lazy
 from django.contrib.auth import views as auth_views
-from .views import mima, emezzeta, lesnina
+from .views import mima, emezzeta, lesnina, prima, harvey, home
 
 app_name = 'furniture' # za url putanju do appa
 
 urlpatterns = [
+     path('home/', home, name='home'),
      path('mima/', mima, name='mima'),
      path('emezzeta/', emezzeta, name='emezzeta'),
      path('lesnina/', lesnina, name='lesnina'),
-
+     path('prima/', prima, name='prima'),
+     path('harvey/', harvey, name='harvey'),
 ]
--- furniture/views.py
+++ furniture/views.py
@@ -71,7 +71,7 @@ def emezzeta(request):
             with open(r"C:\Users\dmanojlovic\Documents\furniture_scraper\furniture_scraper\emezz_result.json", encoding='utf-8') as f:
             #with urllib.request.urlopen("https://randmovie-scraper.herokuapp.com/logs/result.json") as f:
                 jso = json.load(f)
-            print('ovo je id', jso)
+            print('ovo je id')
             data['jsi'] = jso
             data['status'] = status
             #print(data)
@@ -114,8 +114,76 @@ def lesnina(request):
     #print('task na dnu',task)
     return render(request, 'furniture/lesnina.html', {})
 
-# def home(request):
-#     return render(request, 'furniture/home.html', {})
+@csrf_exempt
+@require_http_methods(['POST', 'GET'])  # only get and post
+def prima(request):
+    data = dict()
+    # settings = {
+    #         'FEED_URI': 'result.json',
+    #         'FEED_FORMAT': 'json'
+    #     }
+    task = ''
+    if request.method == 'POST':
+    # task_id = request.GET.get('task_id', None)
+    # print(task_id)
+    # if task_id is None:
+        task = scrapyd.schedule('default', 'prima')
+        return JsonResponse({'task':task})
+    elif request.method == 'GET':
+        task_id = request.GET.get('task_id', None)
+        if task_id is not None:
+            task_id = task_id[:-1]
+            #print(task_id)
+        status = scrapyd.job_status('default', task_id)
+        #print(status)
+        if status == 'finished':
+            with open(r"C:\Users\dmanojlovic\Documents\furniture_scraper\furniture_scraper\prima_result.json", encoding='utf-8') as f:
+            #with urllib.request.urlopen("https://randmovie-scraper.herokuapp.com/logs/result.json") as f:
+                jso = json.load(f)
+            print('ovo je id')
+            data['jsi'] = jso
+            data['status'] = status
+            #print(data)
+            return JsonResponse(data)
+    #print('task na dnu',task)
+    return render(request, 'furniture/prima.html', {})
+
+@csrf_exempt
+@require_http_methods(['POST', 'GET'])  # only get and post
+def harvey(request):
+    data = dict()
+    # settings = {
+    #         'FEED_URI': 'result.json',
+    #         'FEED_FORMAT': 'json'
+    #     }
+    task = ''
+    if request.method == 'POST':
+    # task_id = request.GET.get('task_id', None)
+    # print(task_id)
+    # if task_id is None:
+        task = scrapyd.schedule('default', 'harvey')
+        return JsonResponse({'task':task})
+    elif request.method == 'GET':
+        task_id = request.GET.get('task_id', None)
+        if task_id is not None:
+            task_id = task_id[:-1]
+            #print(task_id)
+        status = scrapyd.job_status('default', task_id)
+        #print(status)
+        if status == 'finished':
+            with open(r"C:\Users\dmanojlovic\Documents\furniture_scraper\furniture_scraper\harvey_result.json", encoding='utf-8') as f:
+            #with urllib.request.urlopen("https://randmovie-scraper.herokuapp.com/logs/result.json") as f:
+                jso = json.load(f)
+            print('ovo je id')
+            data['jsi'] = jso
+            data['status'] = status
+            #print(data)
+            return JsonResponse(data)
+    #print('task na dnu',task)
+    return render(request, 'furniture/harvey.html', {})
+
+def home(request):
+    return render(request, 'furniture/home.html', {})
 
 
 # Create your views here.
